6810a708f4d3b8409084f2b121c681f4508c4e8e
yyhuang2016 <yuanyuanhuang@ou.edu>
2017-01-12 13:46:34 -0600

    replace sd column with mean+sd and mean-sd

--- reformat_to_csv.R
+++ reformat_to_csv.R
@@ -17,6 +17,11 @@ currentdate = Sys.Date()
 firstday = as.numeric(currentdate - as.Date('2011-01-01'))
 lastday = firstday+6
 
+#firstday = as.numeric(as.Date('2016-10-10') - as.Date('2011-01-01'))
+#curday = as.numeric(currentdate - as.Date('2011-01-01'))
+#lastday = curday+6
+
+
 
 setwd(args1)
 #setwd("C:/Users/yuanyuan/Desktop")
@@ -88,28 +93,47 @@ soil_std = apply(soil.mat[firstday:lastday,],1,sd)
 
 days<-seq(currentdate,currentdate+6, "days")
 
+#days<-seq(as.Date('2016-10-10'),currentdate+6, "days")
+
+days<-format(days, format="%m/%d/%Y")
 
 
-data.gpp.weekly<-data.frame(date=days,gpp=gpp_mean,gpp_sd=gpp_std,nee=nee_mean, 
-          nee_sd=nee_std,er=er_mean,er_sd=er_std,foliage=foliage_mean,foliage_sd=foliage_std,
-          wood=wood_mean,wood_sd=wood_std,root=root_mean,root_sd=root_std,soil=soil_mean,
-          soil_sd=soil_std)
+data.gpp.weekly<-data.frame(date=days,gpp=gpp_mean,gpp_high=gpp_mean+gpp_std,gpp_low=gpp_mean-gpp_std,
+                            nee=nee_mean, nee_high=nee_mean + nee_std, nee_low= nee_mean - nee_std, 
+                            er =er_mean,  er_high =er_mean  + er_std,  er_low = er_mean  - er_std,
+                            foliage=foliage_mean,foliage_high = foliage_mean + foliage_std, foliage_low = foliage_mean - foliage_std,
+                            wood=wood_mean,wood_high = wood_mean + wood_std, wood_low = wood_mean - wood_std,
+                            root=root_mean,root_high = root_mean + root_std, root_low = root_mean - root_std,
+                            soil=soil_mean,soil_high = soil_mean + soil_std, soil_low = soil_mean - soil_std)
+
 
 currenthour=format(Sys.time(), "%H")
 currentyear=format(Sys.time(), "%Y")
 #outhour=3
 
-col_header<-c('date','gpp (g m-2 day-1)','gpp_sd','nee (g m-2 day-1)','nee_sd',
-              'er (g m-2 day-1)','er_sd','foliage_C (g m-2)','foliage_C_sd',
-              'wood_C (g m-2)','wood_C_sd','root_C (g m-2)','root_C_sd',
-              'soil_C (g m-2)','soil_C_sd')
+col_header<-c('date','gpp (g m-2 day-1)','gpp_high (g m-2 day-1)', 'gpp_low (g m-2 day-1)',
+              'nee (g m-2 day-1)','nee_high (g m-2 day-1)', 'nee_low (g m-2 day-1)',
+              'er (g m-2 day-1)', 'er_high (g m-2 day-1)',  'er_low (g m-2 day-1)',
+              'foliage_C (g m-2)','foliage_C_high (g m-2)', 'foliage_C_low (g m-2)',
+              'wood_C (g m-2)','wood_C_high (g m-2)', 'wood_C_low (g m-2)',
+              'root_C (g m-2)','root_C_high (g m-2)', 'root_C_low (g m-2)',
+              'soil_C (g m-2)','soil_C_high (g m-2)', 'soil_C_low (g m-2)' )
 
-outtxt_name=paste(currentyear,"-temp-",sprintf("%s",args4),"-co2-",sprintf("%s",args5),".txt",sep='')
-outcsv_name=paste(currentyear,"-temp-",sprintf("%s",args4),"-co2-",sprintf("%s",args5),".csv",sep='')
+#outtxt_name=paste(currentyear,"-temp-",sprintf("%s",args4),"-co2-",sprintf("%s",args5),".txt",sep='')
+#outcsv_name=paste(currentyear,"-temp-",sprintf("%s",args4),"-co2-",sprintf("%s",args5),".csv",sep='')
+
+# modified 01_04_2017, stick on one file for all the years 
+outtxt_name=paste("temp-",sprintf("%s",args4),"-co2-",sprintf("%s",args5),".txt",sep='')
+outcsv_name=paste("temp-",sprintf("%s",args4),"-co2-",sprintf("%s",args5),".csv",sep='')
+
+
 print(outcsv_name)
 
+#write.table(data.gpp.weekly, file = outtxt_name, sep = ",", col.names = col_header,
+#row.names = F, qmethod = "double")
+
 write.table(data.gpp.weekly, file = outtxt_name, sep = ",", col.names = col_header,
-row.names = F, qmethod = "double")
+   row.names = F, quote = FALSE)
 
 file.rename(outtxt_name,outcsv_name)  # in order to add double quote
 
